9016b765aa44c447d53c870bc016ba277f25ee43
Kellar Elliott <kellarelliott@gmail.com>
2020-02-04 16:57:28 +0000

    Constructor Super

--- src/App.js
+++ src/App.js
@@ -5,15 +5,22 @@ import List from './components/Lists';
 import Note from './components/Note';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      showNote: false
+    };
+  }
   render() {
+    const { showNote } = this.state;
     return (
       <div className="App">
         <Nav />
-        <List />
-        <Note />
+        {showNote ? <Note /> : <List />}
       </div>
     );
   }
 }
+
 
 export default App;
