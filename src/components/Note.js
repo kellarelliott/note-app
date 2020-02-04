2a467df8c15b30fd954799922f8f9629dd7e8bfe
kellarelliott <kellarelliott@gmail.com>
2020-02-04 23:01:16 +0000

    close tag form

--- src/components/Note.js
+++ src/components/Note.js
@@ -49,12 +49,28 @@ class Note extends React.Component {
     }
   }
 
+  renderTags(note) {
+    if (note.tags) {
+      return note.tags.map((tag, index) =>
+        <div className="tag" key={index}>
+          <span className="delete">
+            <i className="material-icons">delete</i>
+          </span>
+          {tag.name}
+        </div>
+      );
+    }
+  }
+
   render() {
-    const { note } = this.props;
+    const { note, closeTagForm } = this.props;
 
     return (
       <div className="note-container">
-        <form className="note-form" onSubmit={(e) => this.onSubmit(e)}>
+        <form className="note-form"
+          onSubmit={(e) => this.onSubmit(e)}
+          onClick={() => closeTagForm()}
+        >
           <input
             className="note-title-input"
             type="text"
@@ -73,6 +89,9 @@ class Note extends React.Component {
         <div className="tag-container">
           <div className="tag-button-container">
             {this.renderTagForm(note)}
+          </div>
+          <div className="tag-list-container">
+            {this.renderTags(note)}
           </div>
         </div>
       </div>
