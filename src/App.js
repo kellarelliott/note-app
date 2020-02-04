08ccc8fa5a2deeb7e894e99647739345d6d78647
Kellar Elliott <kellarelliott@gmail.com>
2020-02-04 16:42:32 +0000

    Lists and Note

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
 import Nav from './components/Nav';
+import List from './components/Lists';
+import Note from './components/Note';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Nav />
+        <List />
+        <Note />
       </div>
     );
   }
---
+++ src/components/Note.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Note extends React.Component {
+  render() {
+    return (
+      <div className="note-container">
+        Note Component
+      </div>
+    );
+  }
+
+}
+
+export default Note;
