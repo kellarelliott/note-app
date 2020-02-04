5d3bac8acbbdf0736452eaff38f1b05c2bc89157
kellarelliott <kellarelliott@gmail.com>
2020-02-04 19:21:01 +0000

    edit delete notes

--- src/components/NoteCard.js
+++ src/components/NoteCard.js
@@ -2,9 +2,22 @@ import React from 'react';
 
 class NoteCard extends React.Component {
   render() {
+    const { note } = this.props;
+
     return (
       <div className="note-card-container">
-        NoteCard Component
+        <div className="note-card-title">
+          {note.title}
+        </div>
+        <div className="note-card-content">
+          {note.content}
+        </div>
+        <span className="note-card-delete">
+          <i className="material-icons">close</i>
+        </span>
+        <span className="note-card-edit">
+          <i className="material-icons">mode_edit</i>
+        </span>
       </div>
     );
   }
